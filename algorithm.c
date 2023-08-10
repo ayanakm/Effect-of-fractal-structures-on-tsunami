4b9ad051028da571d386ec352fbba00183a0777a
Ayana Koumura <117553300+ayanakm@users.noreply.github.com>
2023-08-10 20:50:50 +0900

    nanになる

--- algorithm.c
+++ algorithm.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 
 // 初期条件関数 f(x)
-float initialCondition(float x) {
-    return 2 * x * (1 - x);
+double initialCondition(double x) {
+    return 2.0 * x * (1.0 - x);
 }
 
 // 境界条件関数 g(x)
-float boundaryCondition(float x) {
+double boundaryCondition(double x) {
     return 0.0;
 }
 
 int main() {
     // パラメータ設定
-    float T = 100.0;      // 時間上限
+    double T = 100.0;      // 時間上限
     int M = 1000;         // 時間刻み数
-    float L = 1.0;        // 空間幅
+    double L = 1.0;        // 空間幅
     int N = 100;          // 空間刻み数
-    float c = 1.0;        // 波の速さ
-    float h = L / N;      // 空間刻み幅
-    float tau = T / M;    // 時間刻み幅
-    float lamb = c * tau / h; // 安定性条件を満たすための係数
+    double c = 1.0;        // 波の速さ
+    double h = (L * 1.0) / (N * 1.0);      // 空間刻み幅
+    double tau = T / (M * 1.0);    // 時間刻み幅
+    double lamb = c * tau / h; // 安定性条件を満たすための係数
 
     // 配列の宣言と初期化
-    float x[N+2];         // 空間座標
-    float u[N+2];         // u(x, t)
-    float old_u[N+2];     // 前の時刻のu(x, t)
-    float new_u[N+2];     // 次の時刻のu(x, t)
+    double x[N+2];         // 空間座標
+    double u[N+2];         // u(x, t)
+    double old_u[N+2];     // 前の時刻のu(x, t)
+    double new_u[N+2];     // 次の時刻のu(x, t)
 
     // 初期値設定
     for (int j = 1; j <= N; j++) {
