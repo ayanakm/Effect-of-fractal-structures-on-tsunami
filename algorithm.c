933c0bcba32ec89e4f466eb550df34d1baf749fb
Ayana Koumura <117553300+ayanakm@users.noreply.github.com>
2023-08-11 14:13:38 +0900

    Mを10倍、nanを消した

--- algorithm.c
+++ algorithm.c
@@ -13,13 +13,13 @@ double boundaryCondition(double x) {
 int main() {
     // パラメータ設定
     double T = 100.0;      // 時間上限
-    int M = 10000;         // 時間刻み数
+    int M = 1000;         // 時間刻み数
     double L = 1.0;        // 空間幅
     int N = 100;          // 空間刻み数
     double c = 1.0;        // 波の速さ
     double h = (L * 1.0) / (N * 1.0);      // 空間刻み幅
     double tau = T / (M * 1.0);    // 時間刻み幅
-    double lamb = c * tau / h; // 安定性条件を満たすための係数
+    double lamb = c * tau / h; // 安定性条件を満たすように
 
     // 配列の宣言と初期化
     double x[N+2];         // 空間座標
