0f9eb3fce6a6361c3d587307f38800d586993544
Ayana Koumura <117553300+ayanakm@users.noreply.github.com>
2023-08-20 13:11:57 +0900

    微調整

--- KPequation.c
+++ KPequation.c
@@ -12,7 +12,7 @@ double boundaryCondition(double x) {
 
 int main() {
     double T = 10.0; // 時間上限
-    int M = 10000; // 時間刻み数
+    int M = 1000000; // 時間刻み数
     double L = 1.0; // 空間幅(x方向)
     int N = 100; // 空間刻み数(x方向)
     double H = 0.02; // 空間幅(y方向)
@@ -22,8 +22,8 @@ int main() {
     double beta = (H * 1.0) / (S * 1.0); // 空間刻み幅(y方向)
 
 	// 配列の宣言と初期化
-    double x[N+2]; // 空間座標(x方向)
-    // double y[N+2]; //空間座標(y方向)
+	double x[N+2]; // 空間座標(x方向)
+    double y[N+2]; //空間座標(y方向)
     double u[N+2]; // u(x, t)
     double old_u[N+2]; // 前の時刻のu(x, t)
     double new_u[N+2]; // 次の時刻のu(x, t)
@@ -35,11 +35,17 @@ int main() {
 
     // 初期値設定
     for (int j = 1; j <= N; j++) {
-            x[j] = j * alpha - 0.5 * alpha;
-            old_u[j] = initialCondition(x[j]);
-        }
+        x[j] = j * alpha - 0.5 * alpha;
+        old_u[j] = initialCondition(x[j]);
+    }
 
-	//境界条件
+    old_u[0] = -old_u[1];
+    old_u[N+1] = -old_u[N]; //境界条件
+
+    for (int j = 1; j <= N; j++) {
+        u[j] = old_u[j] + tau * boundaryCondition(x[j]);
+    }
+
 	u[0] = -u[1];
     u[N+1] = -u[N]; //境界条件
 
@@ -55,22 +61,23 @@ int main() {
         plus_u[2] = u[2];
 
         for (int i = 1; i <= S; i++) {
-            // y[i] = i * beta - 0.5 * beta;
+            y[i] = i * beta - 0.5 * beta;
+
             // 現在の状態を表示
             for (int j = 1; j <= N; j++) {
-                if (k == 5)
-                    printf("%f %f %f \n", x[j], t[k], u[j]);
+                if (k == 0)
+                    printf("%f %f %f \n", x[j], y[i], u[j]);
             }
-            if (k == 5)
+            if (k == 0)
                 printf("\n");
 
             // 新しい時刻のu(x, t)を計算
             for (int j = 3; j <= N; j++) {
-                            //jの前後の波高（u）の差 (new_u[j-1] - new_u[j+1])
-                            //u[i+1]→plus_u[j]、u[i-1]→minus_u[j
-                            dif_u[j] = old_u[j-1] - old_u[j+1] + (tau / alpha) * ((3.0 / 2.0) * (u[j+1] - u[j-1]) * (u[j+1] - u[j-1]) + 6.0 * u[j] * (u[j+1] - 2.0 * u[j] + u[j-1]) + (1 / alpha * alpha) *(u[j+2] - 4.0 * u[j+1] + 6*u[j] - 4.0 * u[j-1] + u[j-2])) + 3.0 * (alpha * tau /(beta * beta)) * (plus_u[j] - 2.0 * u[j] + minus_u[j]);
-                            //(-1) * dif_uを代入
-                            new_u[j] = (u[j-1] + (-1.0) * dif_u[j-1]) / 2.0;
+                //jの前後の波高（u）の差 (new_u[j-1] - new_u[j+1])
+                //u[i+1]→plus_u[j]、u[i-1]→minus_u[j]
+                dif_u[j] = old_u[j-1] - old_u[j+1] + (tau / alpha) * ((3.0 / 2.0) * (u[j+1] - u[j-1]) * (u[j+1] - u[j-1]) + 6.0 * u[j] * (u[j+1] - 2.0 * u[j] + u[j-1])) + (1.0 / alpha * alpha) * ((u[j+2] - 4.0 * u[j+1] + 6.0 * u[j] - 4.0 * u[j-1] + u[j-2])) + 3.0 * (alpha * tau /(beta * beta)) * (plus_u[j] - 2.0 * u[j] + minus_u[j]);
+                //(-1) * dif_uを代入
+                new_u[j] = (u[j-1] + (-1.0) * dif_u[j-1]) / 2.0;
             }
 
             // 前の時刻のu(x, t)を更新
@@ -82,8 +89,8 @@ int main() {
 
             // u(x, t)を更新
             for (int j = 1; j <= N; j++) {
-                u[j] = new_u[j];
                 //横一列(y方向の影響考えない)ver(★いい感じに動いたら変えるところ★)
+                u[j] = new_u[j];
                 minus_u[j] = u[j];
                 plus_u[j] = u[j];
             }
@@ -91,5 +98,6 @@ int main() {
             u[N+1] = -u[N];
         }
     }
+
     return 0;
 }
